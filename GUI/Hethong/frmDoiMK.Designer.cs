0bef980f39da9ee787265d2768d18456fc4d53a3
minhquanct3 <minhquanct3@gmail.com>
2015-07-12 11:27:57 +0700

    add

--- GUI/Hethong/frmDoiMK.Designer.cs
+++ GUI/Hethong/frmDoiMK.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 348);
+            this.ClientSize = new System.Drawing.Size(574, 303);
             this.Name = "frmDoiMK";
-            this.Text = "frmDoiMK";
+            this.Text = "Thay đổi mật khẩu";
             this.ResumeLayout(false);
 
         }
